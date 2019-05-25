8d2afa152f37fe01aa8e8cf0cb71efa0943a1814
azigo12 <44235619+azigo12@users.noreply.github.com>
2019-05-25 14:25:24 +0200

    Add files via upload

---
+++ Implementacija/E-lections/Model/Glasac.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    class Glasac
+    {
+        public void Glasaj(int idKandidata)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void KandidujSe()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void KandidujSe2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
