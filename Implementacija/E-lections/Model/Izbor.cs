e16256ceec3c12b299adb42a78c989d7d29671a1
azigo12 <azigo1@etf.unsa.ba>
2019-05-25 17:26:45 +0200

    Napravljena klasa Izbor

---
+++ Implementacija/E-lections/Model/Izbor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    enum StatusIzbora
+    {
+        Aktivan,
+        Neaktivan
+    }
+    class Izbor : IObservable, IIzborPrototip
+    {
+        public int Id { get; set; }
+        public DateTime Pocetak { get; set; }
+        private List<GlasackiListic> opcije;
+        public string Opis { get; set; }
+        public string KantonOgranicenje { get; set; }
+        public StatusIzbora Status { get; set; }
+
+        public void Notify()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clone()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
