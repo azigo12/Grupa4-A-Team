3060640da186798e6e454da1aa5c326f1cabb589
azigo12 <azigo1@etf.unsa.ba>
2019-05-25 15:32:07 +0200

    Napravljena klasa Kandidat

---
+++ Implementacija/E-lections/Model/Kandidat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    class Kandidat : KandidatDecorator
+    {
+        private int brojGlasova;
+
+        public void DodajGlas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
