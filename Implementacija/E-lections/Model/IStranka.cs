5ef36375301c8176320ce7f4242cce2cad17d705
azigo12 <azigo1@etf.unsa.ba>
2019-05-25 17:16:57 +0200

    Interfejs IStranka

---
+++ Implementacija/E-lections/Model/IStranka.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    interface IStranka
+    {
+        Stranka KreirajStranku();
+        void IzbrisiStranku();
+    }
+}
