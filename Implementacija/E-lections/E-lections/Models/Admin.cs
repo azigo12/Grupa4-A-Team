6c3013f7550577f7c7579a9c0094155dbc2424db
azigo12 <azigo1@etf.unsa.ba>
2019-05-28 23:43:47 +0200

    Izmjene u organizaciji koda

---
+++ Implementacija/E-lections/E-lections/Models/Admin.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class Admin : IAdmin, IIzbor, IStranka
+    {
+        public string JMBG { get; set; }
+        public string Lozinka { get; set; }
+        public int Id { get; set; }
+        public IzvjestajFactory IzvjestajFactory;
+
+        public Izvjestaj GenerisiIzvjestaj(string tipIzvjestaja)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Admin KreirajAdmina()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void IzbrisiPrivilegije(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IIzbor KreirajIzbor()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ObrisiIzbor(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Stranka KreirajStranku()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void IzbrisiStranku()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
