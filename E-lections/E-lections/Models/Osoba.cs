9a226fee3e5d551f5674b80f60394979c5e32e6d
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-06-11 01:19:37 +0200

    Dodane anotacije.

--- E-lections/E-lections/Models/Osoba.cs
+++ E-lections/E-lections/Models/Osoba.cs
@@ -16,19 +16,23 @@ namespace E_lections.Models
     {
 
         public int ID { get; set; }
-        [Required(ErrorMessage = "Unesite ime")]
+        [Required(ErrorMessage = "Unesite ime"), Display(Name = "Ime")]
         public string Ime { get; set; }
-        [Required(ErrorMessage = "Unesite prezime")]
+        [Required(ErrorMessage = "Unesite prezime"), Display(Name = "Prezime")]
         public string Prezime { get; set; }
-        [DataType(DataType.Date), Required(ErrorMessage = "Unesit datum rođenja")]
+        [DataType(DataType.Date), Required(ErrorMessage = "Unesit datum rođenja"), Display(Name = "Datum rođenja")]
         public DateTime DatumRodjenja { get; set; }
-        [Required(ErrorMessage = "Unesite broj lične karte"),StringLength(9)]
+        [Required(ErrorMessage = "Unesite broj lične karte"),StringLength(9), Display(Name = "Broj lične karte")]
         public string BrojLicneKarte { get; set; }
-        [Required(ErrorMessage = "Unesite svoj JMBG"), StringLength(13)]
+        [Required(ErrorMessage = "Unesite svoj JMBG"), StringLength(13), Display(Name = "JMBG")]
         public string JMBG { get; set; }
+        [Display(Name = "Spol")]
         public Spol Spol { get; set; }
+        [Display(Name = "Lozinka")]
         public string Lozinka { get; set; }
+        [Display(Name = "Ulica")]
         public string Ulica { get; set; }
+        [Display(Name = "Kanton")]
         public string Kanton { get; set; }
 
         //veze sa drugim tabelama
