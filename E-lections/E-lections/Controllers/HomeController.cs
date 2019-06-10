9a1914115b8de847efc7ed5aa60c164bd28cd498
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-06-10 10:28:49 +0200

    Dodana forma za prijavu.

--- E-lections/E-lections/Controllers/HomeController.cs
+++ E-lections/E-lections/Controllers/HomeController.cs
@@ -21,6 +21,11 @@ namespace E_lections.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
