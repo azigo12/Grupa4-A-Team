0b3335fbe2f14d7d89876130c69da01b43bef897
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-06-10 19:04:07 +0200

    Dodan konstruktor za novi controller.

--- E-lections/E-lections/Controllers/IzborController.cs
+++ E-lections/E-lections/Controllers/IzborController.cs
@@ -2,12 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using E_lections.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace E_lections.Controllers
 {
     public class IzborController : Controller
     {
+
+        private ELectionsDbContext context;
+
+        public IzborController(ELectionsDbContext context)
+        {
+            this.context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
