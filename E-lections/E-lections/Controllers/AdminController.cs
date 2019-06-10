64c944a6ad6a87f8aac8a969f79ad73fd810ab82
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-06-10 11:31:49 +0200

    ispravka Admin Controller

--- E-lections/E-lections/Controllers/AdminController.cs
+++ E-lections/E-lections/Controllers/AdminController.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using E_lections.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace E_lections.Controllers
 {
     public class AdminController : Controller
     {
+
         public IActionResult Index()
         {
             return View();
