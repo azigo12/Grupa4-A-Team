5b276324f9c25858f067fb45ca758b37d5910f74
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-06-10 21:42:46 +0200

    dodan controller za korisnike.

---
+++ E-lections/E-lections/Controllers/OsobaController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_lections.Controllers
+{
+    public abstract class OsobaController : Controller
+    {
+        public abstract IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
