293e58a12a29fa5cd166ec0115f4ef1cfb83cef3
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-06-14 19:03:49 +0200

    Izmjene u Kandidat.cs

--- E-lections/E-lections/Models/Kandidat.cs
+++ E-lections/E-lections/Models/Kandidat.cs
@@ -7,13 +7,13 @@ namespace E_lections.Models
 {
     public class Kandidat : KandidatDekorater, IUcesnik
     {
-        private int brojGlasova;
+        public int brojGlasova { get; set; } = 0;
 
         //veze sa ostalim tabelama, 1:1 veza sa Profil,  1:m veza sa glasackim listicem, m:n veza sa BirackoMjesto
-        public int ProfilId { get; set; }
+        public int? ProfilId { get; set; }
         public virtual Profil Profil { get; set; }
 
-        public int GlasackiListicId { get; set; }
+        public int? GlasackiListicId { get; set; }
         public virtual GlasackiListic GlasackiListic { get; set; }
 
 
