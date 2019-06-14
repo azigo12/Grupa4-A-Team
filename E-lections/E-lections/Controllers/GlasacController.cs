d6049dee3ee998552b047063185aab630f71197b
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-06-14 13:47:32 +0200

    Dependency injection contexta u controller.

--- E-lections/E-lections/Controllers/GlasacController.cs
+++ E-lections/E-lections/Controllers/GlasacController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using E_lections.Models;
 
 namespace E_lections.Controllers
 {
     public class GlasacController : Controller
     {
+        private ELectionsDbContext _context;
+
+        public GlasacController(ELectionsDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View("Index", HomeController.currentlyLogged);
