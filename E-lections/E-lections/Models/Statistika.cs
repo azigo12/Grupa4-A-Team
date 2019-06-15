018dca9ea771df1e5aa0d0094cea92ab7ead21f0
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2019-06-15 23:39:16 +0200

    kreirana klasa za statistiku glasanja

---
+++ E-lections/E-lections/Models/Statistika.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_lections.Models
+{
+    public class Statistika
+    {
+        public int ID { get; set; }
+        public int IzborId { get; set; }
+        public virtual Izbor Izbor { get; set; }
+
+        public int GlasoviMusko { get; set; }
+        public int GlasoviZensko { get; set; }
+        public int GlasoviValidni { get; set; }
+        public int GlasoviNevalidni { get; set; }
+        public IDictionary<string, int> GlasoviZaKanton { get; set; }
+    }
+}
