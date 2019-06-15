701b07c066d6dbe405ee60f18eee50cfd5ec5bd6
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2019-06-15 23:59:20 +0200

    metode koje spasavaju glasove za kanton u bazu i iste pretvaraju u mapu

--- E-lections/E-lections/Models/Statistika.cs
+++ E-lections/E-lections/Models/Statistika.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -15,6 +16,38 @@ namespace E_lections.Models
         public int GlasoviZensko { get; set; }
         public int GlasoviValidni { get; set; }
         public int GlasoviNevalidni { get; set; }
-        public IDictionary<string, int> GlasoviZaKanton { get; set; }
+        public string GlasoviZaKanton { get; set; }
+
+        [NotMapped]
+        public IDictionary<string, int> Mapa { get; set; }
+
+        public static string RemoveLast(string text, string character)
+        {
+            if (text.Length < 1) return text;
+            return text.Remove(text.ToString().LastIndexOf(character), character.Length);
+        }
+
+        public string DajGlasove()
+        {
+            string rezultat = "";
+            foreach (KeyValuePair<string, int> entry in Mapa)
+            {
+                rezultat += entry.Key + "," + entry.Value + "/";
+            }
+
+            RemoveLast(rezultat, "/");
+            return rezultat;
+        }
+
+        public void ZabiljeziGlasove(string glasovi)
+        {
+            string[] kantonGlas = glasovi.Split('/');
+            foreach(string s in kantonGlas)
+            {
+                string[] tmp = s.Split(',');
+                Mapa.Add(tmp[0], Int32.Parse(tmp[1]));
+            }
+        }
+
     }
 }
